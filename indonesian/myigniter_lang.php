bec2c918f58f27da244cb1067211be74adbf1524
Asrul Hanafi <hanafi.asrul@gmail.com>
2017-04-20 09:30:28 +0700

    update language

--- indonesian/myigniter_lang.php
+++ indonesian/myigniter_lang.php
@@ -2,20 +2,24 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['Create Amazing CRUD'] = 'Membuat aplikasi CRUD di web, CMS atau E-Commerce dalam hitungan detik';
+$lang['amazing_crud'] = 'Membuat aplikasi CRUD di web, CMS atau E-Commerce dalam hitungan detik';
 $lang['language'] = 'Language';
 $lang['buy'] = 'Beli myIgniter';
 $lang['try'] = 'Coba myIgniter';
 $lang['get_start'] = 'Memulai myIgniter';
 $lang['feature'] = 'FITUR';
 $lang['crud_builder'] = 'CRUD Builder';
-$lang['crud_builder_desc'] = 'Membuat CRUD hanya beberapa detik, buat CRUD tanpa kode hanya buat dan jadi.';
+$lang['crud_builder_desc'] = 'Buat aplikasi CRUD dengan atau tanpa code menggunakan fitur CRUD Generator.';
 $lang['page_builder'] = 'Page Builder';
-$lang['page_builder_desc'] = 'Membuat Page Frontend dan Backend beberapa detik, buat halaman web tanpa code.';
+$lang['page_builder_desc'] = 'Buat halaman frontend atau backend dengan atau tanpa code menggunakan fitur Page Generator.';
 $lang['modular_extension'] = 'Modular Extensions';
-$lang['modular_extension_desc'] = 'Tambahkan Module hanya dengen click, buat module aplikasi web sesuai keinginan.';
+$lang['modular_extension_desc'] = 'Mendukung HMVC yang membuat mudah untuk memanajamen semua fitur aplikasi.';
 $lang['adminlte_theme'] = 'AdminLTE Theme';
 $lang['adminlte_theme_desc'] = 'Dibuat dengan HTML theme terpopuler AdminLTE dari Abdullah Almsaeed.';
+$lang['database'] = 'Database Manager';
+$lang['database_desc'] = 'Buat dan manajemen table mudah dengan menggunakan fitur Database Manager.';
+$lang['wizard_installation'] = 'Wizard Installation';
+$lang['wizard_installation_desc'] = 'Kurangi waktu anda saat instalasi dan konfigurasi dengan menggunakan fitur Wizard Installation.';
 $lang['last_login'] = 'Terakhir masuk';
 $lang['profile'] = 'Profil';
 $lang['signup'] = 'Daftar';
